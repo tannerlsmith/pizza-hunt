41e85beee131a001ecabf5d687f05cadd054050b
tannerlsmith <tanner.smith@uvm.edu>
2021-08-15 20:04:17 -0400

    added routes

--- server.js
+++ server.js
@@ -7,10 +7,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-
 app.use(require('./routes'));
-
-app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
   useFindAndModify: false,
@@ -20,3 +17,5 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
 
 // Use this to log mongo queries being executed!
 mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
